83d320153924bff547c089dda6aa07b0858c42db
Andy Driver <andy.driver@digital.justice.gov.uk>
2015-04-27 17:35:49 +0100

    Add smoketest runner and reporter to cla_common

---
+++ cla_common/smoketest.py
@@ -0,0 +1,31 @@
+import unittest
+
+
+def smoketest(testcase_class):
+
+    suite = unittest.TestLoader().loadTestsFromTestCase(testcase_class)
+    result = unittest.TestResult()
+    suite.run(result)
+
+    status = 'failure'
+    if result.wasSuccessful():
+        status = 'success'
+
+    def format_result(test):
+        test, output = test
+        return {
+            'name': test._testMethodName,
+            'doc': test._testMethodDoc,
+            'output': output
+        }
+
+    return {
+        'result': {
+            'status': status,
+            'tests_run': result.testsRun,
+            'detail': {
+                'errors': map(format_result, result.errors),
+                'failures': map(format_result, result.failures),
+            }
+        }
+    }
