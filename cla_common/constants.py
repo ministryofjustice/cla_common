25089444289cadc702a385b85f280c12c17d0f90
said-moj <45761276+said-moj@users.noreply.github.com>
2020-03-23 12:22:17 +0000

    Flake8: Break long string into multi-line string

--- cla_common/constants.py
+++ cla_common/constants.py
@@ -287,12 +287,21 @@ ECF_OPTIONS = [
     {
         "key": "XFER_TO_RECORDED_MESSAGE",
         "label": "Transferring inbound call to recorded message? Read out the following statement:",
-        "text": '"On closing this call you will hear a recorded message which will contain information to highlight limited circumstances in which legal aid may still be available to you. Thank you [client name] for calling Civil Legal Advice. Goodbye"',
+        "text": """
+        "On closing this call you will hear a recorded message which will contain information to highlight limited
+         circumstances in which legal aid may still be available to you. Thank you [client name] for calling
+         Civil Legal Advice. Goodbye"
+        """,
     },
     {
         "key": "READ_OUT_MESSAGE",
         "label": "Outbound call? Read out the following statement:",
-        "text": '"Legal aid may be available in exceptional circumstances to people whose cases are out of scope where a refusal to fund would breach Human Rights or enforceable European law. You could seek advice from a legal advisor about whether an application might succeed in your case and how to make one. Thank you for calling Civil Legal Advice. Goodbye"',
+        "text": """
+        "Legal aid may be available in exceptional circumstances to people whose cases are out of scope where a refusal
+         to fund would breach Human Rights or enforceable European law. You could seek advice from a legal advisor
+         about whether an application might succeed in your case and how to make one. Thank you for calling
+         Civil Legal Advice. Goodbye"
+        """,
     },
     {"key": "PROBLEM_NOT_SUITABLE", "label": "Problem not suitable for ECF message", "text": ""},
     {"key": "CLIENT_TERMINATED", "label": "Could not provide - client terminated call", "text": ""},
