e1c225f9c0086ed611d50494b7c738d08066e946
said-moj <45761276+said-moj@users.noreply.github.com>
2020-03-23 13:34:26 +0000

    Flake8: Remove unused import

--- cla_common/services/__init__.py
+++ cla_common/services/__init__.py
@@ -1,7 +1,10 @@
 from speaklater import make_lazy_string
+
+
 class BaseAdapter(object):
     _instance = None
     _adapter_factory = None
+
     @classmethod
     def set_adapter_factory(cls, adapter_factory):
         # The staticmethod for the factory is needed because this will be an unbound callable. Not needed for Python3
@@ -35,4 +38,5 @@ def translate(string):
         if adapter:
             return adapter(string_in)
         return string_in
+
     return make_lazy_string(translation_callback, string)
