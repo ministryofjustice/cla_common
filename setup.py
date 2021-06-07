2da49ee8d3e15031f84146339fe3d18e4581ebfa
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2021-06-07 10:49:43 +0100

    Add pytz to package dependencies

--- setup.py
+++ setup.py
@@ -11,44 +11,45 @@ import cla_common
 
 version = cla_common.__version__
 
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
     print("You probably want to also tag the version now:")
     print("  git tag -a %s -m 'version %s'" % (version, version))
     print("  git push --tags")
     sys.exit()
 
-readme = open('README.rst').read()
+readme = open("README.rst").read()
 
 setup(
-    name='cla_common',
+    name="cla_common",
     version=version,
     description="""common code for CLA""",
     long_description=readme,
-    author='MOJ',
-    author_email='kotecha.ravi@gmail.com',
-    url='https://github.com/ministryofjustice/cla_common',
+    author="MOJ",
+    author_email="kotecha.ravi@gmail.com",
+    url="https://github.com/ministryofjustice/cla_common",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'python-dateutil',
-        'django-extended-choices==0.3.0',
-        'requests',
-        'speaklater==1.3',
+        "python-dateutil",
+        "django-extended-choices==0.3.0",
+        "requests",
+        "speaklater==1.3",
+        "pytz==2021.1",
     ],
     license="MIT",
     zip_safe=False,
-    keywords='cla_common',
+    keywords="cla_common",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
+        "Development Status :: 2 - Pre-Alpha",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
     ],
 )
